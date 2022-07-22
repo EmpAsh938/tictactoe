92937ff5b24056b993c001383b0c3d0a6d95cca6
EmpAsh938 <ashokthapa938@gmail.com>
2022-07-22 12:56:02 +0545

    mark for computer grid type checked

--- src/utils/markComputer.ts
+++ src/utils/markComputer.ts
@@ -1,17 +1,17 @@
+import { WinnerMark } from '../context';
 
-
-export const markComputer = (grid:any) => {
-    let emptyGrid = [];
-    let row = grid.length;
-    let col = grid[0].length;
-    for(let i=0;i<row;i++){
-        for(let j=0;j<col;j++){
+export const markComputer = (grid:WinnerMark[][]) => {
+    let emptyGrid:number[][] = [];
+    let row:number = grid.length;
+    let col:number = grid[0].length;
+    for(let i:number=0;i<row;i++){
+        for(let j:number=0;j<col;j++){
             if(grid[i][j] === '') {
-                let newBox = [i,j];
+                let newBox:number[] = [i,j];
                 emptyGrid.push(newBox);
             }
         }
     }
-    if(emptyGrid.length === 0) return new Array(-1,-1,-1);
+    if(emptyGrid.length === 0) return [];
     return emptyGrid[Math.floor(Math.random() * emptyGrid.length)];
 }
